7ed169db78de4ecfcdc29f6288f33ed60ec22777
Francopizzolante <Francogpizzolantesuarez@gmail.com>
2023-09-07 17:00:09 -0300

    update

--- PII_Full_GRASP_and_SOLID_1_Start/src/Library/Recipe.cs
+++ PII_Full_GRASP_and_SOLID_1_Start/src/Library/Recipe.cs
@@ -25,14 +25,31 @@ namespace Full_GRASP_And_SOLID.Library
             this.steps.Remove(step);
         }
 
+        public double GetProductionCost()
+        {
+            double totalCostOfIngredients = 0;
+            double totalCostOfEquipment = 0;
+
+            foreach (Step step in this.steps)
+            {
+                totalCostOfIngredients += step.Quantity * step.Input.UnitCost;
+                totalCostOfEquipment += (step.Time / 60.0) * step.Equipment.HourlyCost; // Convertir minutos a horas
+            }
+            double totalProductionCost = totalCostOfIngredients + totalCostOfEquipment;
+            return totalProductionCost;
+        }
+
         public void PrintRecipe()
         {
             Console.WriteLine($"Receta de {this.FinalProduct.Description}:");
             foreach (Step step in this.steps)
             {
                 Console.WriteLine($"{step.Quantity} de '{step.Input.Description}' " +
-                    $"usando '{step.Equipment.Description}' durante {step.Time}");
+                    $"usando '{step.Equipment.Description}' durante {step.Time} minutos");
             }
+
+            double totalProductionCost = GetProductionCost();
+            Console.WriteLine($"Costo total de producción: ${totalProductionCost}");
         }
     }
 }
